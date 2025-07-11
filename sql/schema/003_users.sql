07a4c3ca883864759ae457b289ef8366f12cba8e
edipretoro <edipretoro@gmail.com>
2025-07-11 22:10:43 +0200

    Adding a uid column to the users table

--- sql/schema/003_users.sql
+++ sql/schema/003_users.sql
@@ -1,6 +1,7 @@
 -- +goose Up
 create table if not exists users (
     id uuid primary key,
+    uid integer not null unique,
     username text not null unique,
     fullname text not null unique,
     created_at timestamp not null default (CURRENT_TIMESTAMP),
