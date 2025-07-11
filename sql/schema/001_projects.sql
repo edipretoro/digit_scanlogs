5630ec716b20aaf519e53462cc9db1087503a23b
edipretoro <edipretoro@gmail.com>
2025-07-11 22:11:41 +0200

    Adding a new column, created_by, to the projects table

--- sql/schema/001_projects.sql
+++ sql/schema/001_projects.sql
@@ -4,6 +4,7 @@ create table if not exists projects (
   name text not null,
   path text not null,
   description text,
+  created_by uuid references users(id) on delete set null,
   created_at timestamp not null default (CURRENT_TIMESTAMP),
   updated_at timestamp not null default (CURRENT_TIMESTAMP),
   delete_at timestamp
