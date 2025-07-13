6b70307b0f70f517eb22805cedff0cb15d8c0361
edipretoro <edipretoro@gmail.com>
2025-07-13 09:19:17 +0200

    Configuring the access to the database

--- cmd/digit_scanlogs/main.go
+++ cmd/digit_scanlogs/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
+
+	_ "modernc.org/sqlite"
 
 	"github.com/joho/godotenv"
 )
@@ -12,6 +15,16 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
+	dbURL := os.Getenv("DIGIT_SCAN_DSN")
+	if dbURL == "" {
+		log.Fatal("DIGIT_SCAN_DSN environment variable is not set")
+	}
+	db, err := sql.Open("sqlite", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening database: %v", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
 	scanDirectory := os.Getenv("DIGIT_SCAN_DIR")
 	if scanDirectory == "" {
 		log.Fatal("DIGIT_SCAN_DIR environment variable is not set")
