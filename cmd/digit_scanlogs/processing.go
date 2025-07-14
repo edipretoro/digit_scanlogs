f0d55008d603c93d0003684839d091fd91edb407
edipretoro <edipretoro@gmail.com>
2025-07-14 16:48:49 +0200

    First iteration to save information about the projects in the database

--- cmd/digit_scanlogs/processing.go
+++ cmd/digit_scanlogs/processing.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"path/filepath"
 	"github.com/edipretoro/digit_scanlogs/internal/database"
-	"github.com/google/uuid"
 )
 
 func IsDigitProject(projectPath string) bool {
@@ -37,8 +36,60 @@ func processingScanDirectory(scanDir string, dbQueries *database.Queries) error 
 		if project.IsDir() {
 			projectPath := fmt.Sprintf("%s/%s", scanDir, project.Name())
 			if IsDigitProject(projectPath) {
+				metadata, err := os.Stat(projectPath)
+				if err != nil {
+					return fmt.Errorf("failed to get metadata for project %s: %w", project.Name(), err)
+				}
+				metadataStat := metadata.Sys().(*syscall.Stat_t)
 				fmt.Printf("Found Digit project: %s\n", project.Name())
-				err = processProject(projectPath)
+				fmt.Printf("Project metadata: UID=%d, GID=%d, Size=%d bytes\n", metadataStat.Uid, metadataStat.Gid, metadata.Size())
+				userUid := strconv.FormatUint(uint64(metadataStat.Uid), 10)
+				user, err := user.LookupId(userUid)
+				if err != nil {
+					return fmt.Errorf("failed to lookup user for UID %d: %w", metadataStat.Uid, err)
+				}
+				var userDb database.User
+				userDb, err = dbQueries.GetUserByUID(context.Background(), int64(metadataStat.Uid))
+				if err != nil {
+					if err == sql.ErrNoRows {
+						userDb, err = dbQueries.CreateUser(
+							context.Background(),
+							database.CreateUserParams{
+								ID:       uuid.NewString(),
+								Uid:      int64(metadataStat.Uid),
+								Username: user.Username,
+								Fullname: user.Name,
+							},
+						)
+						if err != nil {
+							return fmt.Errorf("failed to create user in database: %w", err)
+						}
+					} else {
+						return fmt.Errorf("failed to get user by UID %d: %w", metadataStat.Uid, err)
+					}
+				}
+				var projectDb database.Project
+				projectDb, err = dbQueries.GetProjectByPath(context.Background(), projectPath)
+				if err != nil {
+					if err == sql.ErrNoRows {
+						projectDb, err = dbQueries.CreateProject(
+							context.Background(),
+							database.CreateProjectParams{
+								ID:          uuid.NewString(),
+								Name:        project.Name(),
+								Path:        projectPath,
+								Description: sql.NullString{},
+								CreatedBy:   userDb.Uid,
+							},
+						)
+						if err != nil {
+							return fmt.Errorf("failed to create project in database: %w", err)
+						}
+					} else {
+						return fmt.Errorf("failed to get project by path %s: %w", projectPath, err)
+					}
+				}
+				err = processProject(projectDb)
 				if err != nil {
 					return fmt.Errorf("failed to process project %s: %w", project.Name(), err)
 				}
