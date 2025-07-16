2de1235321177dff8fc47c73c96a9570adb7f965
edipretoro <edipretoro@gmail.com>
2025-07-16 14:21:48 +0200

    Refactoring the magefile to simplify the building and deployment process

--- magefile.go
+++ magefile.go
@@ -5,13 +5,14 @@ package main
 
 import "github.com/magefile/mage/sh"
 
+var platforms = map[string]string{
+	"linux":   "amd64",
+	"darwin":  "arm64",
+	"windows": "amd64",
+}
+
 // Building binaries for the various systems it should run on.
 func Build() {
-	platforms := map[string]string{
-		"linux":   "amd64",
-		"darwin":  "arm64",
-		"windows": "amd64",
-	}
 	for os, arch := range platforms {
 		suffix := ""
 		if os == "windows" {
@@ -29,6 +30,14 @@ func Clean() {
 
 // Copy the binaries to the arcnum server
 func Deploy() {
-	sh.Run("scp", "build/digitscanlogs-windows*.exe", "rarcnum:")
-	sh.Run("scp", "build/digitscanlogs-linux*", "rarcnum:")
+	for os, arch := range platforms {
+		if os == "darwin" {
+			continue
+		}
+		suffix := ""
+		if os == "windows" {
+			suffix = ".exe"
+		}
+		sh.Run("scp", "build/digitscanlogs-"+os+"-"+arch+suffix, "rarcnum:")
+	}
 }
